1e949206ec90c5050de3c1681e5ec7df4f19fa43
cindychem <135210673+cindychem@users.noreply.github.com>
2023-06-12 04:13:02 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,19 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+word = input( '請輸入中文字:' )
+url = f'https://dict.revised.moe.edu.tw/search.jsp?md=1&word={word}#searchL'
 
-def read ( word ):
-    url = f'https://crptransfer.moe.gov.tw/index.jsp?SN={word}&sound=1#res'
-
-    html = requests.get( url )
-    bs = BeautifulSoup(html.text,'lxml')
-    data = bs.find('th', axis='資料類型')
-    try:
-        row = data.find_all('tr')[3]
-        chinese = row.find('tr').text
-        phones = row.find_all('span')
-        phone = [e.text for e in phones]
-        s = " ".join( phone )
-        # s = row.find('sub')
-        return( chinese + s)
-    except:
-        return( '查無此字' )
+html = requests.get( url )
+bs = BeautifulSoup(html.text,'lxml')
+data = bs.find('table', id='searchL')
+try:
+    row = data.find_all('tr')[2]
+    chinese = row.find('cr').text
+    phones = row.find_all('code')
+    phone = [e.text for e in phones]
+    s = " ".join( phone )
+    # s = row.find('sub')
+    print( c, s )
+except:
+    print( '查無此字' )
