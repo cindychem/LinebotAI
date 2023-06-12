bd56a8bcc50b052ab8673fe719e6881b1f4918ba
cindychem <135210673+cindychem@users.noreply.github.com>
2023-06-12 21:41:58 +0800

    Update new.py

--- new.py
+++ new.py
@@ -16,6 +16,6 @@ def read( word ):
         phone = [e.text for e in phones]
         s = " ".join( phone )
         # s = row.find('sub')
-        return( chinese + s )
+        return( chinese + \n s )
     except:
         return( '查無此字' )
